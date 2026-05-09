3e2488ad2f7e2026c723420f4ef7d4e4e774ebec
Dominik Panek <33105114+psychoo102@users.noreply.github.com>
2026-05-09 04:56:11 +0200

    Addendum

---
+++ src/CFunctions.cpp
@@ -0,0 +1,29 @@
+#include "CFunctions.h"
+#include <random>
+#include <uuid.h>
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_io.hpp>
+
+using namespace uuids;
+
+// ====================== UUID v4 ======================
+int CFunctions::uuid4(lua_State* luaVM)
+{
+    if (!luaVM) return 0;
+	boost::uuids::uuid uuid = boost::uuids::random_generator()();
+	std::string uuid_str = boost::uuids::to_string(uuid);
+	lua_pushstring(luaVM, uuid_str.c_str());
+    return 1;
+}
+
+// ====================== UUID v7 ======================
+int CFunctions::uuid7(lua_State* luaVM)
+{
+    if (!luaVM) return 0;
+    //uuid const id = uuids::uuid_system_generator{}();
+    boost::uuids::uuid uuid = boost::uuids::time_generator_v7()();
+	std::string uuid_str = boost::uuids::to_string(uuid);
+	lua_pushstring(luaVM, uuid_str.c_str());
+    return 1;
+}
---
+++ src/ml_uuid.cpp
@@ -0,0 +1,79 @@
+/*********************************************************
+*
+*  Multi Theft Auto: San Andreas - Deathmatch
+*
+*  ml_base, External lua add-on module
+*  
+*  Copyright © 2003-2008 MTA.  All Rights Reserved.
+*
+*  Grand Theft Auto is © 2002-2003 Rockstar North
+*
+*  THE FOLLOWING SOURCES ARE PART OF THE MULTI THEFT
+*  AUTO SOFTWARE DEVELOPMENT KIT AND ARE RELEASED AS
+*  OPEN SOURCE FILES. THESE FILES MAY BE USED AS LONG
+*  AS THE DEVELOPER AGREES TO THE LICENSE THAT IS
+*  PROVIDED WITH THIS PACKAGE.
+*
+*********************************************************/
+
+#include "ml_uuid.h"
+#include "luaimports.h"
+#include <signal.h>
+
+ILuaModuleManager10 *pModuleManager = NULL;
+bool ms_bInitWorked = false;
+
+// Initialisation function (module entrypoint)
+MTAEXPORT bool InitModule ( ILuaModuleManager10 *pManager, char *szModuleName, char *szAuthor, float *fVersion )
+{
+	pModuleManager = pManager;
+
+	// Set the module info
+	strncpy ( szModuleName, MODULE_NAME, MAX_INFO_LENGTH );
+	strncpy ( szAuthor, MODULE_AUTHOR, MAX_INFO_LENGTH );
+	(*fVersion) = MODULE_VERSION;
+
+    if ( !ImportLua() )
+    {
+        return false;
+    }
+
+    ms_bInitWorked = true;
+	return true;
+}
+
+
+MTAEXPORT void RegisterFunctions ( lua_State * luaVM )
+{
+    if ( !ms_bInitWorked )
+        return;
+
+	if ( pModuleManager && luaVM )
+	{
+        // Register functions
+		pModuleManager->RegisterFunction(luaVM, "uuidv4", CFunctions::uuid4);
+		pModuleManager->RegisterFunction(luaVM, "uuidv7", CFunctions::uuid7);
+	}
+}
+
+MTAEXPORT void PlayerJoined ( lua_State * luaVM, int iPlayerID, char* szPlayerName )
+{
+
+}
+
+MTAEXPORT bool DoPulse ( void )
+{
+	return true;
+}
+
+
+MTAEXPORT void ResourceStopped ( lua_State * luaVM )
+{
+}
+
+
+MTAEXPORT bool ShutdownModule ( void )
+{
+    // Stop all sockets before shutting down the module
+	return true;
+}
